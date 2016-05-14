3ba78e80ec044c6d68e35c324cbea4c4c9864386
Phillip Dornauer <phillip@dornauer.cc>
2016-05-14 15:09:50 +0200

    added profile edit view

--- www/js/app.js
+++ www/js/app.js
@@ -5,27 +5,57 @@ phonon.options({
         enableBrowserBackButton: true,
         templateRootDirectory: '../pages'
     },
-    i18n: null // for this example, we do not use internationalization
+    i18n: null
 });
 
 
+/**
+ * 
+ * This data should come form a remote server
+ * 
+ */
+var user = {
+    email: 'phillip@dornauer.cc',
+    nickname: 'phillip.dornauer',
+    points: 2000
+};
+
 var app = phonon.navigator();
 
-/**
- * The activity scope is not mandatory.
- * For the home page, we do not need to perform actions during
- * page events such as onCreate, onReady, etc
-*/
-app.on({page: 'home', preventClose: false, content: null});
+app.on({page: 'home', preventClose: false, content: null}, function(activity){
 
-/**
- * However, on the second page, we want to define the activity scope.
- * [1] On the create callback, we add tap events on buttons. The OnCreate callback is called once.
- * [2] If the user does not tap on buttons, we cancel the page transition. preventClose => true
- * [3] The OnReady callback is called every time the user comes on this page,
- * here we did not implement it, but if you do, you can use readyDelay to add a small delay
- * between the OnCreate and the OnReady callbacks
-*/
+    activity.onReady(function() {
+        for( var attr in user ){
+            document.querySelector("[data-profile-" + attr + "]").textContent = user[attr];
+        }
+    });
+    
+});
+
+
+app.on({page: 'page-profile-edit', content: 'page-profile-edit.html'}, function(activity) {
+    
+    var $email, $nickname;
+    
+    activity.onReady(function(self) {
+        $email.value = user.email;
+        $nickname.value = user.nickname;
+    });
+    
+    activity.onCreate(function(self){
+        
+        $email = document.querySelector("#input-email");
+        $nickname = document.querySelector("#input-nickname");
+        
+        document.querySelector("#action-save").on("tap", function(){
+            user.email = $email.value;
+            user.nickname = $nickname.value;
+        });
+    });
+    
+});
+
+/*
 app.on({page: 'pagetwo', preventClose: true, content: 'pagetwo.html', readyDelay: 1}, function(activity) {
 
     var action = null;
@@ -62,7 +92,6 @@ app.on({page: 'pagetwo', preventClose: true, content: 'pagetwo.html', readyDelay
     activity.onHashChanged(function(pizza) {
         document.querySelector('.pizza').textContent = pizza;
     });
-});
+});*/
 
-// Let's go!
 app.start();
